286b5f24d3b55599538b19d8587f85c70fd9d60d
Kevin Aloisi <kaloisi@gmail.com>
2025-07-27 19:35:30 -0400

    v1l

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,24 @@ class Line {
   toPath() : string {
     return "M" + this.start.x + " " + this.start.y + " L" + this.end.x + " " + this.end.y
   }
+
+  getLineLengthInPixels() : number {
+    let h = Math.abs(this.start.y - this.end.y)
+    let w = Math.abs(this.start.x - this.end.x)
+    return Math.round(Math.sqrt(Math.pow(h,2) + Math.pow(w,2)))
+  }
+
+  getMidPoint() : Point {
+    return new Point(
+      this.start.x + (this.end.x - this.start.x)/2,
+      this.start.y + (this.end.y - this.start.y)/2
+    )
+  }
+
+  getLineLengthInFeet(ruler: Line, inFeet: number) : number {
+    let pixPerFoot = ruler.getLineLengthInPixels() / inFeet
+    return Math.round(this.getLineLengthInPixels() / pixPerFoot)
+  }
 }
 
 class Point {
@@ -37,6 +55,7 @@ interface AppProps {
 interface AppState {
   url: string,
   scaleInFeet: number,
+  updateRuler: boolean,
   ruler: Line
   scale: Line
   dragLine: Line
@@ -48,6 +67,7 @@ class App extends React.Component<AppProps, AppState> {
     super(props)
     this.state = {
       isDragging: false,
+      updateRuler: true,
       scaleInFeet: 20,
       url: "/plot.png",
       ruler: new Line(new Point(0,0), new Point(0, 100)),
@@ -62,7 +82,7 @@ class App extends React.Component<AppProps, AppState> {
       this.setState({
         dragLine: new Line(
           this.state.dragLine?.start,
-          new Point(e.clientX, e.clientY),
+          new Point(e.nativeEvent.offsetX, e.nativeEvent.offsetY),
         )
       })
     }
@@ -72,21 +92,58 @@ class App extends React.Component<AppProps, AppState> {
     this.setState({
       isDragging: true,
       dragLine: new Line(
-        new Point(e.clientX, e.clientY),
-        new Point(e.clientX, e.clientY),
+        new Point(e.nativeEvent.offsetX, e.nativeEvent.offsetY),
+        new Point(e.nativeEvent.offsetX, e.nativeEvent.offsetY),
       )
     })
   }
 
-  renderLine(line : Line | null, color: string) {
-    let path = line?.toPath()
+  saveChange() {
+    if (this.state.updateRuler) {
+      this.setState({ 
+        isDragging:false,
+        ruler: this.state.dragLine,
+        updateRuler: false
+      })
+    } else {
+      this.setState({ 
+        isDragging:false,
+        scale: this.state.dragLine,
+      })
+    }
+  }
+
+  renderLine(line : Line, color: string) {
+    let path = line.toPath()
+    let midPoint = line.getMidPoint()
+    let textStyle = {
+        fill: 'green',
+        fontSize: 24
+        
+    }
     return (
-      <path style={{
-        stroke: color,
-        strokeWidth: 2,
-      }} d={path}/>
+      <>
+        <path style={{
+          stroke: color,
+          strokeWidth: 4,
+        }} d={path}/>
+        <text style={textStyle}
+           x={midPoint.x}
+           y={midPoint.y}>{line.getLineLengthInPixels()}px</text>
+        <text style={textStyle}
+           x={midPoint.x}
+           y={midPoint.y + 30}>{line.getLineLengthInFeet(this.state.ruler, this.state.scaleInFeet)}ft</text>
+      </>
     )
   }
+
+  updateScaleInFeet(e: React.ChangeEvent<HTMLInputElement>) {
+    let newValue = e.target.value
+    this.setState({
+      scaleInFeet: parseFloat(newValue)
+    })
+  }
+  
 
   render() {
     return (<>
@@ -95,7 +152,7 @@ class App extends React.Component<AppProps, AppState> {
       <FormGroup>
           <Slider aria-label="Image Scale" value={10} />
           <TextField value={this.state.url}/>
-          <TextField value={this.state.scaleInFeet}/>
+          <TextField value={this.state.scaleInFeet} onChange={(e) => this.updateScaleInFeet(e)}/>
         </FormGroup>
       </Box>
 
@@ -103,12 +160,12 @@ class App extends React.Component<AppProps, AppState> {
             <svg width={1526} height={1526}
               onMouseDown={(e) => this.recordClick(e)}
               onMouseMove={(e) => this.mouseMove(e)}
-              onMouseUp={() => this.setState({ isDragging:false })}
+              onMouseUp={(e) => this.saveChange()}
               >
                 <image href={this.state.url} width='1536' />
-                {this.renderLine(this.state.dragLine, 'red')}
-                {this.renderLine(this.state.ruler, 'blue')}
-                {this.renderLine(this.state.scale, 'green')}
+                {this.state.isDragging && this.renderLine(this.state.dragLine, 'red')}
+                {!this.state.isDragging && this.renderLine(this.state.ruler, 'blue')}
+                {!this.state.isDragging && this.renderLine(this.state.scale, 'green')}
             </svg>
         </div>
       </ThemeProvider>
